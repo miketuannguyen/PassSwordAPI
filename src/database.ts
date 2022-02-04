597972db198a5cbc07328681072bfac9fdbdaa86
nguyen.luu <nguyen.luu@flex.com.vn>
2022-02-04 21:53:52 +0700

    refactor: apply singleton to MySQLConnection

--- src/database.ts
+++ src/database.ts
@@ -1,49 +1,51 @@
-import { UserEntity } from './entities';
 import { Connection, createConnection } from 'typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import { UserEntity } from './entities';
 
-export class MySQLDatabase {
-    private static _connection: Connection;
+export class MySQLConnection {
+    /** MySQL connection singleton instance */
+    private static _instance: MySQLConnection;
+
+    /** MySQL database connection options */
+    private _connectionOptions: MysqlConnectionOptions;
+
+    /** MySQL database connection */
+    public connection: Connection;
 
     /** Constructor */
-    private constructor() {}
+    private constructor() {
+        this._connectionOptions = {
+            type: 'mysql',
+            host: process.env.MYSQL_DB_HOST,
+            port: Number(process.env.MYSQL_DB_PORT),
+            username: process.env.MYSQL_DB_USERNAME,
+            password: process.env.MYSQL_DB_PASSWORD,
+            database: process.env.MYSQL_DB_DATABASE,
+            entities: [UserEntity],
+        };
+    }
+
+    /**
+     * Get MySQL connection singleton instance
+     * @returns MySQL connection
+     */
+    public static getInstance(): MySQLConnection {
+        if (!this._instance) {
+            this._instance = new MySQLConnection();
+        }
+        return this._instance;
+    }
 
     /**
      * Create MySQL connection
      * @returns success / fail
      */
-    public static async createConnection(): Promise<boolean> {
+    public async createConnection(): Promise<boolean> {
         try {
-            this._connection = await createConnection({
-                type: 'mysql',
-                host: '192.168.64.3',
-                port: 3306,
-                username: 'root',
-                password: '123',
-                database: 'pass-sword',
-                entities: [UserEntity]
-            });
+            this.connection = await createConnection(this._connectionOptions);
             return true;
         } catch (e) {
             return false;
         }
-    }
-
-    /**
-     * Get MySQL entity manager
-     * @returns MySQL entity manager
-     */
-    public static async getManager() {
-        if (!this._connection) {
-            this._connection = await createConnection({
-                type: 'mysql',
-                host: '192.168.64.3',
-                port: 3306,
-                username: 'root',
-                password: '123',
-                database: 'pass-sword',
-                entities: [UserEntity]
-            });
-        }
-        return this._connection.manager;
     }
 }
